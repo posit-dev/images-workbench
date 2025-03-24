8c585dce6fa53db8119f701a7763bcfe8aea75a2
Ian Pittwood <ian.pittwood@rstudio.com>
2025-03-24 14:12:03 -0600

    Add `hkps` protocol to gpg keys call

--- workbench/2024.12.0+467.pro1/scripts/install_workbench.sh
+++ workbench/2024.12.0+467.pro1/scripts/install_workbench.sh
@@ -19,7 +19,8 @@ curl -fsSL -o /tmp/workbench.deb ${WORKBENCH_DOWNLOAD_URL}
 
 # Verify Workbench package
 echo "$d Verify Workbench package $d"
-gpg --keyserver keys.openpgp.org --recv-keys 51C0B5BB19F92D60
+
+gpg --keyserver hkps://keys.openpgp.org --recv-keys 51C0B5BB19F92D60
 dpkg-sig --verify /tmp/workbench.deb
 
 # Patch Workbench package to not attempt service startup
