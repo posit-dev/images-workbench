bb616d4f270eea56ca5fb2a3f93af20766365f95
Ian Pittwood <ian.pittwood@rstudio.com>
2025-09-19 09:41:44 -0600

    Add latest and dev versions

---
+++ workbench/2025.09/scripts/install_workbench.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# Output delimiter
+d="===="
+
+# Update apt repositories
+apt-get update -yq
+
+echo "$d Install Posit Workbench 2025.09.0+387.pro2 $d"
+
+RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1 apt-get install -yf rstudio-server=2025.09.0+387.pro2
+apt-mark hold rstudio-server
+
+# Clean up
+apt-get clean -yqq && \
+rm -rf /var/lib/apt/lists/*
