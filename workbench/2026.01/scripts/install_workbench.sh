361d28b8f2d6c38965f552d90676ab1321a28d2f
Ian H. Pittwood <ian.pittwood@posit.co>
2026-02-24 09:22:52 -0700

    Patch Workbench 2026.01.0 to 2026.01.1

--- workbench/2026.01/scripts/install_workbench.sh
+++ workbench/2026.01/scripts/install_workbench.sh
@@ -7,9 +7,9 @@ d="===="
 # Update apt repositories
 apt-get update -yq
 
-echo "$d Install Posit Workbench 2026.01.0+392.pro5 $d"
+echo "$d Install Posit Workbench 2026.01.1+403.pro11 $d"
 
-RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1 apt-get install -yf rstudio-server=2026.01.0+392.pro5
+RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1 apt-get install -yf rstudio-server=2026.01.1+403.pro11
 apt-mark hold rstudio-server
 
 # Clean up
